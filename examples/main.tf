fd5446c256b71828203f972f641a3de72f61939d
spartan-quannguyen <quan.nguyen@c0x12c.com>
2025-08-05 11:29:22 +0700

    Upgrade default Jenkins plugins to prevent crashes and patch CVE-2025-47884

--- examples/main.tf
+++ examples/main.tf
@@ -9,18 +9,18 @@ module "eks_helm_jenkins" {
   github_org                     = "spartan"
   github_app_oauth_client_id     = "spartan"
   github_app_oauth_client_secret = "super-secure"
-  jenkins_shared_lib_repo        = "github.com/example/example-jenkins-shared-lib.git"
+  jenkins_shared_lib_repo        = "infra-jenkins"
   general_secrets = {
     "secret" = "value"
   }
-  jenkins_base_agent_image_repo = "jenkins/jenkins-agent"
+  jenkins_base_agent_image_repo = "jenkins"
   jenkins_base_agent_image_name = "jenkins-agent"
   jenkins_base_agent_image_tag  = "latest"
   efs_id                        = "fs-12345678"
   jenkins_env_var               = "jenkins-env-var"
   enabled_slack_notification    = false
   enabled_github_app_login      = true
-  jenkins_admins                = ["spartan-P00006-admin"]
+  jenkins_admins                = ["spartan-P00006-admin", "spartan-P00006-iaas"]
   jenkins_executors             = ["spartan-P00006-leader", "spartan-P00006-member"]
 
   enabled_init_scripts = true
